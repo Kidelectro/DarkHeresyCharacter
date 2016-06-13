a0ec12e0fcf9526a58e9f2d3fda1f0b4c7f78352
Marshall Myers <marshall.myers@mobiwm.com>
2016-06-12 20:36:11 -0400

    Feature: add index and group views

---
+++ app/controllers/intro_page_controller.rb
@@ -0,0 +1,6 @@
+class IntroPageController < ApplicationController
+
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   resources :skills, only: [:index, :show]
   resources :characters, only: [:index, :show]
 
-  root 'characters#index'
+  resources :users, only: [:new, :create]
+
+  resources :user_sessions, only: [:create, :destroy]
+
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
+  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
+  get '/sign_in', to: 'user_sessions#new', as: :sign_in
+  root 'intro_page#index'
 end
